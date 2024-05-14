aedbad4ca1220fe312d065061834437327a1f6a6
Marcus <hjerpemarcus@gmail.com>
2024-05-14 12:29:06 +0200

    push

--- scripts/home.js
+++ scripts/home.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const observerOptions = {
         root: null, 
         rootMargin: '0px',
-        threshold: 0
+        threshold: 0.5
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
